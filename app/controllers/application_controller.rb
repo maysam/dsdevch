2b6a98ef77e8cb77478c27e4a468aa39322e5522
maysam <kherad@gmail.com>
2016-06-10 06:14:12 +0430

    map view seems working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def usecase1
-    @venues = Venue.all
+    @venues = Venue.where.not(latitude: nil, longitude: nil).select(:id, :name, :longitude, :latitude).all
   end
 
   def usecase2
-    @venues = Venue.all
+    @venues = Venue.where.not(latitude: nil, longitude: nil).select(:id, :name, :longitude, :latitude).all
   end
 end
