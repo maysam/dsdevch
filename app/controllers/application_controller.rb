205b3b1f5860ffae4c5a17dfcbbf98ef0cbea073
maysam <kherad@gmail.com>
2016-06-12 01:12:05 +0430

    putting extra fields in the map popup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def usecase1
-    @venues = Venue.where.not(latitude: nil, longitude: nil).select(:id, :name, :longitude, :latitude)
+    @venues = Venue.where.not(latitude: nil, longitude: nil).select(:id, :name, :street, :postcode, :city, :housenumber, :tourism, :longitude, :latitude)
   end
 
   def usecase2
